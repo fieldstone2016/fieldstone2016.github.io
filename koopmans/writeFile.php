263eeb8e9ac350bf0cf08e5cd12459516007e859
eKoopmans <koopmans13@hotmail.com>
2016-05-30 11:11:18 -0400

    Discussion updates

--- koopmans/writeFile.php
+++ koopmans/writeFile.php
@@ -2,6 +2,7 @@
 
 // Allow this proxy to be accessed from anywhere
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Max-Age: 86400");
 
 // Check the password
 $password = $_POST['password']	or die('No password given.');
@@ -9,7 +10,7 @@ if ($password != 'ICS4U')		die('Invalid password.');
 
 // Gather necessary info
 $filename = $_POST['filename']	or die('No filename specified.');
-$data = $_POST['data']			or die('No data specified.');
+$data = $_POST['data'];
 
 // Write info to file
 file_put_contents($filename, json_encode($data,TRUE))	or die('Write failed.');
